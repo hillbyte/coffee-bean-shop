d33142369fd50a776ccc62014d08b4e8dd7e94c6
hillbyte <ahillbaky@gmail.com>
2021-08-03 11:41:26 +0530

    product model updated

--- store-backend/models/product.js
+++ store-backend/models/product.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 32,
+      maxlength: 80,
     },
     description: {
       type: String,
